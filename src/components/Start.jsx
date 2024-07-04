79f0da12548f9a60140b3471823e8774e011c8d7
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-07-04 11:04:18 +0900

    로딩화면 추가 및 이전 결과 보기

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,15 +1,17 @@
-import { useNavigate } from 'react-router-dom';
-import bannerImage from '../image/짱구배너.png';
+import { useNavigate } from "react-router-dom";
+import bannerImage from "../image/짱구배너.png";
 
 function Start() {
   const navigate = useNavigate();
+  const mbtiResult = sessionStorage.getItem("mbti") || "결과 없음";
 
   return (
     <div className="content" id="start">
       <p>나와 닮은 짱구 캐릭터 테스트</p>
       <img src={bannerImage} className="img" />
       <p id="maker">제작자: 이나경</p>
-      <button className="btn btn-warning" onClick={() => navigate('/question')}>
+      <p id="maker">이전결과: {mbtiResult}</p>
+      <button className="btn btn-warning" onClick={() => navigate("/question")}>
         떡잎마을로 출발~
       </button>
     </div>
